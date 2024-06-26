3aed1835437125f10c8b7adf355c6a1b214db086
hansVelarde11 <hsssvf@gmail.com>
2024-06-25 22:40:33 -0500

    Agregar Middleware Validate
    -Se agrego el middleware a las rutas de user

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,21 +6,27 @@ const userController = require("../controllers/userController");
 const validateRequest = require('../middlewares/validateRequest')
 const { registerSchema } = require('../validation/userValidation/registerSchema')
 const { updateSchema } = require('../validation/userValidation/updateSchema')
+const { deleteSchema } = require('../validation/userValidation/deleteSchema')
+const { getAllUsersSchema } = require('../validation/userValidation/getAllUsersSchema')
+const { getPetsByUserSchema } = require('../validation/userValidation/getPetsByUserSchema')
+const { getUserByIdSchema } = require('../validation/userValidation/getUserByIdSchema')
+const { savePreferencesSchema } = require('../validation/userValidation/savePreferences')
+
 
 //POST
 router.post("/", validateRequest(registerSchema) , userController.register);
 
 //PATCH
 router.patch("/:id", validateRequest(updateSchema), userController.update);
-router.patch("/:id/settings", userController.savePreferences);
+router.patch("/:id/settings", validateRequest(savePreferencesSchema) , userController.savePreferences);
 
 //DELETE
-router.delete("/:id", userController.deleteUser);
-
+router.delete("/:id", validateRequest(deleteSchema), userController.deleteUser);
+                 
 //GET
-router.get("/", userController.getAllUsers);
-router.get('/:id', userController.getUserById)
-router.get('/:id/pets', userController.getPetsByUser
+router.get("/", validateRequest(getAllUsersSchema), userController.getAllUsers);
+router.get('/:id', validateRequest(getUserByIdSchema), userController.getUserById)
+router.get('/:id/pets', validateRequest(getPetsByUserSchema),userController.getPetsByUser
 )
 
 module.exports = router
