a3d3a0dacd41ba29a393bedc7d9725bd3cbc0e00
hansVelarde11 <hsssvf@gmail.com>
2024-06-25 23:54:06 -0500

    Se implementó el middleware de validacion
    -En las rutas de etiquetas se implemento el middleware usando joi

--- api/routes/etiqueta.js
+++ api/routes/etiqueta.js
@@ -2,19 +2,29 @@ const express = require("express");
 const router = express.Router();
 const etiquetaController = require("../controllers/etiquetaController");
 
+//Validaciones
+const validateRequest = require('../middlewares/validateRequest')
+const {deleteSchema} = require('../validation/etiquetaValidation/deleteSchema ')
+const {getAllPetsByTagsSchema} = require('../validation/etiquetaValidation/getAllPetsByTagsSchema ')
+const {getAllPostsByTagSchema} = require('../validation/etiquetaValidation/getAllPostsByTagSchema ')
+const {getAllTagsSchema} = require('../validation/etiquetaValidation/getAllTagsSchema ')
+const {getTagByNameSchema} = require('../validation/etiquetaValidation/getTagByNameSchema ')
+const {registerSchema} = require('../validation/etiquetaValidation/registerSchema')
+const {updateSchema} = require('../validation/etiquetaValidation/updateSchema')
+
 //GET
-router.get("/pets", etiquetaController.getAllPetsByTags);
-router.get("/", etiquetaController.getAllTags);
-router.get("/:id/posts", etiquetaController.getAllPostsByTag);
-router.get("/:name", etiquetaController.getTagByName);
+router.get("/pets", validateRequest(getAllPetsByTagsSchema), etiquetaController.getAllPetsByTags);
+router.get("/", validateRequest(getAllTagsSchema),etiquetaController.getAllTags);
+router.get("/:id/posts", validateRequest(getAllPostsByTagSchema),etiquetaController.getAllPostsByTag);
+router.get("/:name", validateRequest(getTagByNameSchema), etiquetaController.getTagByName);
 
 //POST
-router.post("/", etiquetaController.register);
+router.post("/", validateRequest(registerSchema), etiquetaController.register);
 
 //PATCH
-router.patch("/:id", etiquetaController.update);
+router.patch("/:id", validateRequest(updateSchema), etiquetaController.update);
 
 //DELETE
-router.delete("/:id", etiquetaController.deleteTag);
+router.delete("/:id", validateRequest(deleteSchema), etiquetaController.deleteTag);
 
 module.exports = router
